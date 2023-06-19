0fbc5c544dc59bc3110ed64ac792a7e6a933d494
Theo <theo.rodrigues62120@gmail.com>
2023-06-19 17:15:53 +0200

    four commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const jokeElement = document.getElementById('joke');
 const getJokeButton = document.getElementById('getJokeButton');
 
 getJokeButton.addEventListener('click', () => {
-  fetch('/api/jokes/random')
+  fetch('https://blagueback.onrender.com/api/jokes/random')
     .then(response => response.json())
     .then(data => {
       jokeElement.textContent = data.question + ' ' + data.answer;
