b59e888b3ac4a9d1127df89a6e43c08060a98e38
Theo <theo.rodrigues62120@gmail.com>
2023-06-19 16:18:16 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const jokeElement = document.getElementById('joke');
+const getJokeButton = document.getElementById('getJokeButton');
+
+getJokeButton.addEventListener('click', () => {
+  fetch('/api/jokes/random')
+    .then(response => response.json())
+    .then(data => {
+      jokeElement.textContent = data.question + ' ' + data.answer;
+    })
+    .catch(error => {
+      jokeElement.textContent = 'Erreur lors de la récupération de la blague.';
+      console.error(error);
+    });
+});
